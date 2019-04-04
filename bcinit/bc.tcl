c50025765c2ba639d4c024cd34ff5d84a9a583b3
ankovcom <a@n-kov.com>
2019-04-04 15:52:09 +0200

    Blockchain experiment init

---
+++ bcinit/bc.tcl
@@ -0,0 +1,12 @@
+#/bin/sh
+#\
+exec wish "$0" "$@"
+
+# An experiment for blockchain-based
+# software/data project management.
+
+# (c) Alexey Noskov 2019 http://n-kov.com
+
+package require n-kov
+
+
